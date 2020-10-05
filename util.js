347c6af049609c7ccc8ee565f3b44add4f4ad60d
trapcodeio <trapcode.io@gmail.com>
2020-10-05 09:58:23 +0100

    Updated xpress-mongo and default model file

--- util.js
+++ util.js
@@ -1,20 +1,13 @@
 const {Client, XMongoModel, is, parseServerUrl} = require('xpress-mongo')
 const config = $.$config.get('mongodb');
 
-if(!config)
+if (!config)
     throw Error('mongodb config not found!');
 
 const XMongoConnection = Client(config.url, config.options);
-
-exports.DBCollection = (collection) => {
-    return class extends XMongoModel {
-        static thisCollection() {
-            return XMongoConnection.collection(collection)
-        }
-    }
-}
 
 exports.XMongoConnection = XMongoConnection;
 exports.XMongoModel = XMongoModel;
 exports.is = is;
 exports.parseServerUrl = parseServerUrl;
+exports.DBCollection = (collection) => XMongoConnection.model(collection);
